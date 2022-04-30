e368576deeb07a3757b4b711a1e7bdc6d7d1d5e2
LelisCis <leliuscris@gmail.com>
2022-04-30 14:55:35 -0300

    modificaciones en rutas de carpetas

--- js/agregarProductos.js
+++ js/agregarProductos.js
@@ -10,7 +10,7 @@ const agregar=()=>{
     
         <div class="agregarImg">
             <div class="arrastrarImg">
-                <img src="/img/Vector.png">
+                <img src="img\Vector.png">
                 <p>Arrastre para agregar una imagen para el producto</p>
             </div>
             <div class="arrastrarBoton">
